7ccc14f8896b4d4f0a704d3707d3978d0c490b80
baphus <sarsonasjosephuskim@gmail.com>
2025-05-10 12:33:13 +0800

    edit

--- Finals/Scripts/page.js
+++
@@ -1,5 +0,0 @@
-// Placeholder script for future interactivity (optional)
-document.querySelector('#vinyl').addEventListener('click', function() {
-    alert('Vinyl clicked!');
-  });
-  
---
+++ page.js
@@ -0,0 +1,5 @@
+// Placeholder script for future interactivity (optional)
+document.querySelector('#vinyl').addEventListener('click', function() {
+    alert('Vinyl clicked!');
+  });
+  
